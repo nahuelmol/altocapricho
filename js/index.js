c61eae4e34c08d0576250147683b1d32a5f2ae0d
Silvio Molina <molinahuel44@gmail.com>
2022-01-25 01:04:00 -0300

    adding icon

--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
-const showCatalog = () => {
-	var nos = document.getElementById('nosotros-content')
-	var promos = document.getElementById('promos-content')
-	var home = document.getElementById('home-content')
-	var contacto = document.getElementById('contacto')
-
-	nos.classList.add('to_null');
-	promos.classList.add('to_null');
-	home.classList.add('to_null');
-	contacot.classList.add('to_null');
-}
-
-document.getElementById('catalog').addEventListener(showCatalog)
+//const showCatalog = () => {
+//	var nos = document.getElementById('nosotros-content')
+//	var promos = document.getElementById('promos-content')
+//	var home = document.getElementById('home-content')
+//	var contacto = document.getElementById('contacto')
+//
+//	var catalog = document.getElementById('catalog')
+//	catalog.classList.add('justShow')
+//
+//	nos.classList.add('to_null');
+//	promos.classList.add('to_null');
+//	home.classList.add('to_null');
+//	contacot.classList.add('to_null');
+//}
+//
+//const showHome = () => {
+//	var nos = document.getElementById('nosotros-content')
+//	var promos = document.getElementById('promos-content')
+//	var catalog = document.getElementById('catalog-content')
+//	var contacto = document.getElementById('contacto')
+//
+//	var home = document.getElementById('home-content')
+//	home.classList.add('justShow')
+//
+//	nos.classList.add('to_null');
+//	promos.classList.add('to_null');
+//	catalog.classList.add('to_null');
+//	contacot.classList.add('to_null');
+//}
+//
+//
+//document.getElementById('catalog').addEventListener("click", showCatalog)
+//document.getElementById('home').addEventListener("click", showHome)
